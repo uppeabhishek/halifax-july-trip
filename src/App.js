2b21ee2e172bb1728406fd8cd5d31450a3e35162
Abhishek Uppe <ab577089@dal.ca>
2024-07-08 11:31:10 -0700

    Mai July Trip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { Carousel } from 'react-responsive-carousel';
 import './App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import PointPleasantPark from "./images/PointPleasantPark.jpg";
+import SunsetWithMalak from "./images/SunsetWithMalak.jpg";
+import SweetHeavenBalls from "./images/SweetHeavenBalls.jpg";
+import AngelSunsetWithText from "./images/AngelSunsetWithText.png";
+
+function TripCarousel() {
+        return (
+            <Carousel autoPlay interval="5000" transitionTime="1000" showThumbs={false}>
+                <div>
+                    <img src={SweetHeavenBalls} width={500} height={500} />
+                    <p className="legend">Sweet Heaven Balls</p>
+                </div>
+                <div>
+                    <img src={PointPleasantPark} width={500} height={500}/>
+                    <p className="legend">Point Pleasant Park</p>
+                </div>
+                <div>
+                    <img src={SunsetWithMalak} width={500} height={500} />
+                    <p className="legend">Sunset With Malak</p>
+                </div>
+                <div>
+                    <img src={AngelSunsetWithText} width={500} height={500} />
+                    <p className="legend">Sunset</p>
+                </div>
+            </Carousel>
+        );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Hiiiii Malakkkkk <span role="img" aria-label="sheep">😊</span>
+                    <div>
+                        It was so nice meeting with you, I created this page just as a memory in July :)
+                    </div>
+                    <div>
+                        Don't forget to look at last image.
+                    </div>
+                </p>
+                <TripCarousel />
+            </header>
+        </div>
+    );
 }
 
 export default App;
